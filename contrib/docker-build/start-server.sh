27d04da70b1675d25f1e2a99d51beddcf3ef6225
benjaminmnoer <benjaminmnoer25@gmail.com>
2023-12-22 22:37:42 +0100

    Separated running of realm and world processes in script

--- contrib/docker-build/start-server.sh
+++ contrib/docker-build/start-server.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-2>/dev/null 1>/dev/null ./mangosd &
-2>/dev/null 1>/dev/null ./realmd &
+if [ $mangosd -eq 1 ]
+then
+    echo "Starting mangosd process"
+    2>/dev/null 1>/dev/null ./mangosd &
+    echo "PID $!"
+fi
+
+if [ $realmd -eq 1 ]
+then
+    echo "Starting realmd process"
+    2>/dev/null 1>/dev/null ./realmd &
+    echo "PID $!"
+fi
+
 sleep infinity
