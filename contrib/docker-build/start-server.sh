add9103d84a0251c88d15eb1a36f223775b09185
Benjamin Noer <benjaminmnoer25@gmail.com>
2024-01-02 20:24:11 +0100

    Updated script to shutdown gracefully

--- contrib/docker-build/start-server.sh
+++ contrib/docker-build/start-server.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
 
+MPID=0
+RPID=0
+TERMINATING=0
+
+_term() {
+    echo "Sigterm caught in $BASHPID, halting running server processes..."
+
+    if [[ $MPID -gt 0 && $TERMINATING -eq 0 ]]
+    then
+        TERMINATING=1
+        echo "Stopping mangosd PID: $MPID"
+        kill -TERM "$MPID" 2>/dev/null
+        wait "$MPID"
+    fi
+
+    if [[ $RPID -gt 0 && $TERMINATING -eq 0 ]]
+    then
+        TERMINATING=1
+        echo "Stopping realmd PID: $RPID"
+        kill -TERM "$RPID" 2>/dev/null
+        wait "$RPID"
+    fi
+}
+
+trap _term SIGTERM
+
 if [ $mangosd -eq 1 ]
 then
     echo "Starting mangosd process"
     2>/dev/null 1>/dev/null ./mangosd &
-    echo "PID $!"
+    echo "mangosd started with PID $!"
+    MPID=$!
 fi
 
 if [ $realmd -eq 1 ]
 then
     echo "Starting realmd process"
     2>/dev/null 1>/dev/null ./realmd &
-    echo "PID $!"
+    echo "realmd started with PID $!"
+    RPID=$!
 fi
 
-sleep infinity
+if [ $MPID -gt 0 ]
+then 
+    wait "$MPID"
+fi
+
+if [ $RPID -gt 0 ]
+then
+    wait "$RPID"
+fi
